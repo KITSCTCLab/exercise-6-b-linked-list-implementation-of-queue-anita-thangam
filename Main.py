e83ca3d0566655e6a963c42ffe3db077b67d5b2c
anita-thangam <110010649+anita-thangam@users.noreply.github.com>
2022-09-06 12:06:25 +0530

    anita

--- Main.py
+++ Main.py
@@ -20,14 +20,14 @@ class Queue:
 
 
 queue = Queue()
-operations[];
-for specific_operation in input().split(','):
+operations[]
+for specific_operation in input().split(',')
     operations.append(specific_operation.strip())
 input_data = input()
 data = input_data.split(',')
-for i in range(len(operations)):
-  if operations[i] == "enqueue":
+for i in range(len(operations))
+  if operations[i] == "enqueue"
     queue.enqueue(int(data[i]))
-  elif operations[i] == "dequeue":
+  elif operations[i] == "dequeue"
     queue.dequeue()
 queue.status()
